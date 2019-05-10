e45d3b80a167f576e27e3c775b78ff2aa3cc440b
HTC <a77sabeghi@gmail.com>
2019-05-10 11:41:11 +0430

    Reading data from database

--- app/src/main/java/com/example/androidexamples/MainActivity.java
+++ app/src/main/java/com/example/androidexamples/MainActivity.java
@@ -2,12 +2,66 @@ package com.example.androidexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.androidexamples.adapter.ProductListAdapter;
+import com.example.androidexamples.database.DatabaseHelper;
+import com.example.androidexamples.model.Product;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView lvProduct;
+    private File database;
+    private List<Product> productList;
+    private ProductListAdapter adapter;
+    private DatabaseHelper mDBHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDBHelper = new DatabaseHelper(this);
+        lvProduct = findViewById(R.id.lvProduct);
+
+        //check if database exists
+        database = getApplicationContext().getDatabasePath(DatabaseHelper.DBNAME);
+        if(database.exists() == false){
+            if(copyDatabase())
+                Toast.makeText(this, "Succesfully copied", Toast.LENGTH_SHORT).show();
+            else {
+                Toast.makeText(this, "Error in copy database", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        productList = mDBHelper.getProductList();
+        adapter = new ProductListAdapter(this,productList);
+        lvProduct.setAdapter(adapter);
+    }
+
+    boolean copyDatabase(){
+        try {
+            InputStream inputStream = this.getAssets().open(DatabaseHelper.DBNAME);
+            OutputStream outputStream = new FileOutputStream(database.getPath());
+            byte[] buffer = new byte[1024];
+            int length = 0;
+            while ((length = inputStream.read(buffer))>0){
+                outputStream.write(buffer);
+            }
+            outputStream.flush();
+            outputStream.close();
+            Log.v("MainActivity","db copied");
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/androidexamples/database/DatabaseHelper.java
+++ app/src/main/java/com/example/androidexamples/database/DatabaseHelper.java
@@ -1,10 +1,16 @@
 package com.example.androidexamples.database;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.example.androidexamples.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseHelper {
-    public static final String DBNAME = "sample.db";
+    public static final String DBNAME = "sample2.db";
     private Context mContext;
     private SQLiteDatabase mDatabase;
 
@@ -21,5 +27,20 @@ public class DatabaseHelper {
     private void closeDatabase(){
         if (mDatabase!=null)
             mDatabase.close();
+    }
+    public List<Product> getProductList(){
+        List<Product> productList = new ArrayList<>();
+        Product product = null;
+        openDatabase();
+        Cursor cursor = mDatabase.rawQuery("SELECT * FROM product",null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()){
+            product = new Product(cursor.getInt(0),cursor.getString(1),cursor.getInt(2),cursor.getString(3));
+            productList.add(product);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        closeDatabase();
+        return productList;
     }
 }
