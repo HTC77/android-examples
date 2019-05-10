db81468a34024805d230199303091ef7bd1ac4b2
HTC <a77sabeghi@gmail.com>
2019-05-10 14:21:51 +0430

    init with greenDao

---
+++ app/src/main/java/com/example/androidexamples/App.java
@@ -0,0 +1,33 @@
+package com.example.androidexamples;
+
+import android.app.Application;
+
+import com.example.androidexamples.database.DaoMaster;
+import com.example.androidexamples.database.DaoSession;
+
+import org.greenrobot.greendao.database.Database;
+
+public class App extends Application {
+
+    private DaoSession daoSession;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // regular SQLite database
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "notes-db");
+        Database db = helper.getWritableDb();
+
+        // encrypted SQLCipher database
+        // note: you need to add SQLCipher to your dependencies, check the build.gradle file
+        // DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, "notes-db-encrypted");
+        // Database db = helper.getEncryptedWritableDb("encryption-key");
+
+        daoSession = new DaoMaster(db).newSession();
+    }
+
+    public DaoSession getDaoSession() {
+        return daoSession;
+    }
+}
---
+++ app/src/main/java/com/example/androidexamples/NoteType.java
@@ -0,0 +1,5 @@
+package com.example.androidexamples;
+
+public enum NoteType {
+    TEXT, LIST, PICTURE
+}
