777df26d7c165b277bad112511a3fe31d6a1db1a
HTC <a77sabeghi@gmail.com>
2019-05-17 11:33:01 +0430

    Selecting image from gallery

--- app/src/main/java/com/example/androidexamples/MainActivity.java
+++ app/src/main/java/com/example/androidexamples/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.androidexamples;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnChooseImg, btnUpload;
+    private EditText etName;
+    private ImageView imageView;
+    private final int IMG_REQUEST = 1;
+    private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnChooseImg = findViewById(R.id.btnChoose);
+        btnUpload = findViewById(R.id.btnUpload);
+        etName = findViewById(R.id.etName);
+        imageView = findViewById(R.id.imageView);
+        btnChooseImg.setOnClickListener(this);
+        btnUpload.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnChoose:
+                selectImage();
+                break;
+            case R.id.btnUpload:
+
+                break;
+            }
+    }
+
+    private void selectImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,IMG_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMG_REQUEST &&
+                resultCode == RESULT_OK && data!= null){
+            Uri path = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),path);
+                imageView.setImageBitmap(bitmap);
+                imageView.setVisibility(View.VISIBLE);
+                etName.setVisibility(View.VISIBLE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
