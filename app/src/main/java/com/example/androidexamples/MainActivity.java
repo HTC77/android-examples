4d8cc7929910427cd338d799017863d2f1a5b752
HTC <a77sabeghi@gmail.com>
2019-05-17 13:25:28 +0430

    Uploading Image

--- app/src/main/java/com/example/androidexamples/MainActivity.java
+++ app/src/main/java/com/example/androidexamples/MainActivity.java
@@ -7,18 +7,33 @@ import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnChooseImg, btnUpload;
     private EditText etName;
     private ImageView imageView;
     private final int IMG_REQUEST = 1;
+    private final String UPLOAD_URL = "http://10.0.3.2/ImageUpload/updateinfo.php";
     private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 selectImage();
                 break;
             case R.id.btnUpload:
-
+                uploadImage();
                 break;
             }
     }
@@ -66,5 +81,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 e.printStackTrace();
             }
         }
+    }
+
+    private void uploadImage(){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                UPLOAD_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    String response = jsonObject.getString("response");
+                    Toast.makeText(MainActivity.this, response,
+                            Toast.LENGTH_LONG).show();
+                    imageView.setImageResource(0);
+                    imageView.setVisibility(View.GONE);
+                    etName.setText("");
+                    etName.setVisibility(View.GONE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(MainActivity.this, "Error occured", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", etName.getText().toString().trim());
+                params.put("image",imageToBase64String(bitmap));
+                return params;
+            }
+        };
+        MySingleton.getInstance(MainActivity.this).addToRequestQueue(stringRequest);
+    }
+
+    private String imageToBase64String(Bitmap bitmap){
+        ByteArrayOutputStream byteArrayOutputStream =
+                new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+        byte[] imgBytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imgBytes,Base64.DEFAULT);
     }
 }
